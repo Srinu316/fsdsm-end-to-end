3295d922a1132f8f49a46aa2a43ed6ec78c93619
Bhushan_crestere <naga@crestere.io>
2024-09-08 16:47:39 +0530

    changed

--- init_setup.sh
+++ init_setup.sh
@@ -9,10 +9,10 @@ conda create --prefix ./env python=3.8 -y
 
 echo [$(date)]: "activating the environment" 
 
-source activate ./env
+conda activate ./env
 
 echo [$(date)]: "installing the dev requirements" 
 
 pip install -r requirements.txt
 
-echo [$(date)]: "END"
+echo [$(date)]: "END" 
