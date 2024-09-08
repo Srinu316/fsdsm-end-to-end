910df39ef1a56348da1b841e5f2464d373d9ec38
Bhushan_crestere <naga@crestere.io>
2024-09-08 18:15:04 +0530

    changed

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 from setuptools import find_packages,setup
 
 setup(
-    name='DimondPricePrediction',
+    name='DiamondPricePrediction',
     version='0.0.1',
-    author='sunny savita',
-    author_email='sunny.savita@ineuron.ai',
+    author='Srinu',
+    author_email='srinivaskenguva316@gmail.com',
     install_requires=["scikit-learn","pandas","numpy"],
     packages=find_packages()
-)
+) 
